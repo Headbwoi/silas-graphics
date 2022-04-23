d41c112f960963a79a1edb668c849a28d9a2ec27
headbwoi <tochukwujohn24@gmail.com>
2022-04-23 21:34:41 +0100

    finished header styling

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    fontFamily: {
+      mono: ["'Space Mono'", "monospace"],
+    },
+    extend: {
+      colors: {
+        dark: "hsl(217, 28%, 15%)",
+        veryDark: "hsl(216, 53%, 9%)",
+        lightDark: "hsl(218, 28%, 13%)",
+        veryLightDark: "hsl(219, 30%, 18%)",
+      },
+    },
+  },
+  plugins: [],
+};
