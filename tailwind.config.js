c0fff9e9db55f2f57692f462976508fe2da79606
headbwoi <tochukwujohn24@gmail.com>
2022-04-26 18:49:39 +0100

    finished hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,21 @@ module.exports = {
     fontFamily: {
       mono: ["'Space Mono'", "monospace"],
     },
+    container: {
+      center: true,
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+    },
     extend: {
       colors: {
         dark: "hsl(217, 28%, 15%)",
         veryDark: "hsl(216, 53%, 9%)",
         lightDark: "hsl(218, 28%, 13%)",
         veryLightDark: "hsl(219, 30%, 18%)",
+        crimson:"crimson",
       },
     },
   },
