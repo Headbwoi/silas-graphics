1b6ef105fe67fc9666179086e7823063dfa60c5d
headbwoi <tochukwujohn24@gmail.com>
2022-04-30 13:22:43 +0100

    added intersection observer

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const toggle = document.getElementById("toggle")
+const cards = document.querySelectorAll(".card")
 
 toggle.onclick = () => {
     toggle.classList.toggle("active")
 }
+
+const observer = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            entry.target.classList.toggle("show", entry.isIntersecting)
+        })
+    },
+    {
+        threshold: 0.5,
+    }
+)
+
+cards.forEach(card => {
+    observer.observe(card)
+})
